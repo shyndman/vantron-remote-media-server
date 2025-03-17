5211db25b5a9750b4a6132a83cf65f77ec3ba328
Scott Hyndman <scotty.hyndman@gmail.com>
2025-03-17 09:19:49 -0400

    Remove file loggerr

--- src/vantron_remote_media_server/__main__.py
+++ src/vantron_remote_media_server/__main__.py
@@ -11,15 +11,13 @@ from .server import MediaPlayerServer, get_server_config
 logger.remove()  # Remove default handler
 logger.add(
     sys.stderr,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    format=(
+        "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+        "<level>{level: <8}</level> | "
+        "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+        "<level>{message}</level>"
+    ),
     level="INFO",
-)
-logger.add(
-    "vantron-media-server.log",
-    rotation="1 day",
-    retention="7 days",
-    compression="zip",
-    level="DEBUG",
 )
 
 
